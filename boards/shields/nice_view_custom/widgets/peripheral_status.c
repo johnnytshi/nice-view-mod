2b14750f885295b7226baab4c93f9b1820a1ad02
Johnny <example@myemail.com>
2025-04-07 15:52:24 -0700

    custom text as image

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -114,10 +114,10 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_label_create(widget->obj);
-    lv_label_set_text(art, "Hello, World!");
+    lv_obj_t *art = lv_img_create(widget->obj);
+    bool random = sys_rand32_get() & 1;
+    lv_img_set_src(art, random ? &balloon : &mountain);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_obj_set_style_transform_angle(art, 1800, 0);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
